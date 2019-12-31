44d60553ecce82d3957d23a19af8082037cddf2a
LittleBigProgramming <20643587+LittleBigProgramming@users.noreply.github.com>
2019-12-31 15:15:43 +0000

    Added context of a Stylist into class and allowed passing in Stylist instance when the Calendar ojbect is initialised.

--- calendar.rb
+++ calendar.rb
@@ -1,18 +1,29 @@
 # frozen_string_literal: true
 
 class Calendar
+  def initialize(stylist)
+    @stylist = stylist
+  end
+
   def manual_position
-    new_record? ? salon.highest_manual_order_index + 1 : manual_order_index
+    @stylist.new_record? ? salon.highest_manual_order_index + 1 : @stylist.manual_order_index
   end
 
   def alphabetical_position
     stylist_names = salon.ordered_stylist_names
-    stylist_names << name unless stylist_names.member? name
-    stylist_names.map(&:downcase).sort!.index(name.downcase)
+    stylist_names << @stylist.name unless stylist_names.member? @stylist.name
+    stylist_names.map(&:downcase).sort!.index(@stylist.name.downcase)
   end
 
   def order_index
-    return -1 unless active
-    salon.has_manual_order? ? manual_position : alphabetical_position
+    return -1 unless @stylist.active
+
+    @stylist.salon.has_manual_order? ? @stylist.manual_position : @stylist.alphabetical_position
+  end
+
+  private
+
+  def salon
+    @stylist.salon
   end
 end

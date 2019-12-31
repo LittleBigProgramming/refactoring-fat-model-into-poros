f8c4b987ed305a44fff7eafe1dfa3f2e6b966a55
LittleBigProgramming <20643587+LittleBigProgramming@users.noreply.github.com>
2019-12-31 16:02:53 +0000

    The only two methods that need to be called by the are the order_index and the initialize method which can be used whereever needed since it has been decoupled from the Stylist ActiveRecord model.

--- calendar_stylist_list_item.rb
+++ calendar_stylist_list_item.rb
@@ -5,6 +5,14 @@ class CalendarStylistListItem
     @stylist = stylist
   end
 
+  def order_index
+    return -1 unless @stylist.active
+
+    salon_has_manual_order? ? @stylist.manual_position : @stylist.alphabetical_position
+  end
+
+  private
+
   def manual_position
     @stylist.new_record? ? highest_manual_order_index + 1 : @stylist.manual_order_index
   end
@@ -14,14 +22,6 @@ class CalendarStylistListItem
     stylist_names << @stylist.name unless stylist_names.member? @stylist.name
     stylist_names.map(&:downcase).sort!.index(@stylist.name.downcase)
   end
-
-  def order_index
-    return -1 unless @stylist.active
-
-    salon_has_manual_order? ? @stylist.manual_position : @stylist.alphabetical_position
-  end
-
-  private
 
   def highest_manual_order_index
     salon.stylists.map(&:manual_order_index).max
